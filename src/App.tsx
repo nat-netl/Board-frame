5429170853023b0561cc4c7ac97836fb3164edd5
nat-netl <nat_netl3@bk.ru>
2022-08-29 16:24:17 +0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ import Footer from "./components/footer/Footer";
 function App() {
   return (
     <>
-      <Header />
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </Router>
-      <Footer />
     </>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import s from "./header.module.scss";
+import logo from './../../assets/images/logo_transparent.png'
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className={s.header}>
+      <div className={s.wrapper}>
+        <div className={s.logo__wrapper}>
+          <Link to="/">
+            <img src={logo} alt="header-logo" />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
