b3d210e5a7c4d3c04f29f18855871e482a43e452
nat-netl <nat_netl3@bk.ru>
2022-09-07 14:25:18 +0300

    footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import s from '../../assets/styles/main.module.scss'
+import React from "react";
+import m from "../../assets/styles/main.module.scss";
+import NavRoutes from "../navRoutes/NavRoutes";
+import s from "./footer.module.scss";
 
 function Footer() {
+  const currentData = new Date().getFullYear() as number;
+
   return (
-    <footer className={s.container}>
-      Footer
+    <footer className={s.footer}>
+      <div className={m.container}>
+        <div className={s.footer__wrapper}>
+          <div className={s.nav__rows}>
+            <div className={s.nav__row}>
+              <NavRoutes title="Магазин" />
+            </div>
+          </div>
+
+          <div className={s.copyright}> 
+            <div className={s.text__copyright}>&copy; 2022 - {currentData} БордФрейм</div>
+          </div>
+        </div>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
