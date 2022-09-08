3ae370c3ddc3e8fea622d9590facae0af33993b7
nat-netl <nat_netl3@bk.ru>
2022-09-08 11:09:28 +0300

    basket icon in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,17 +3,23 @@ import { Link } from "react-router-dom";
 import s from "./header.module.scss";
 import logo from "./../../assets/images/logo_transparent.png";
 import Navbar from "./navbar/Navbar";
+import LocalMallIcon from '@mui/icons-material/LocalMall';
 
 function Header() {
   return (
     <header className={s.header}>
       <div className={s.wrapper}>
-        <div className={s.logo__wrapper}>
-          <Link to="/">
-            <img src={logo} alt="header-logo" />
-          </Link>
+        <div className={s.wrapper__nav}>
+          <div className={s.logo__wrapper}>
+            <Link to="/">
+              <img src={logo} alt="header-logo" />
+            </Link>
+          </div>
+          <Navbar />
         </div>
-        <Navbar />
+        <div className={s.basket}>
+          <Link to="/basket"><LocalMallIcon /></Link>
+        </div>
       </div>
     </header>
   );
