e7b58da93311bedcb7c31aa9eb377cef62f55528
nat-netl <nat_netl3@bk.ru>
2022-09-09 16:04:39 +0300

    price formatter

--- src/components/products/productIdCard/ProductIdCard.tsx
+++ src/components/products/productIdCard/ProductIdCard.tsx
@@ -9,6 +9,7 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import { IProduct } from "../../../types/card";
 import BuyGreenButton from "../../ui/buttons/buy-button/BuyGreenButton";
 import Instock from "../../ui/instock/Instock";
+import { priceFormatter } from "../../../helpers/formatter/priceFormatter";
 
 function ProductIdCard() {
   const { id }: any = useParams();
@@ -19,6 +20,9 @@ function ProductIdCard() {
   const isExistsInCard = basket.some(
     (p) => p.id === (product && product[0].id)
   );
+  const RUB = priceFormatter('RUB', product && product[0].price);
+  
+  console.log (RUB)
 
   return (
     <div className={s.product}>
@@ -57,7 +61,7 @@ function ProductIdCard() {
               }
             >
               {product && (
-                <span className={m.addToBasket}>{product[0].price} ₽</span>
+                <span className={m.addToBasket}>{RUB}</span>
               )}
 
               {isLoading
