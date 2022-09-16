4305b2acbfa764cfc0eb7b39b6050dc9adefdca0
nat-netl <nat_netl3@bk.ru>
2022-09-16 17:30:47 +0300

    search: seach form component

--- src/components/search/searchForm/SearchForm.tsx
+++ src/components/search/searchForm/SearchForm.tsx
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, FC } from "react";
 import InputSearch from "../../ui/inputs/inputSearch/InputSearch";
 import s from "./seachForm.module.scss";
 import InputSearchBlueButton from "../../ui/inputs/inputSearchBlueButton/InputSearchBlueButton";
 
-const SearchForm = ({ ...props }) => {
-  const [value, setValue] = useState<any>("");
+interface ISearchFormProps {
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void,
+  setSearchValue: (value: string) => void 
+}
 
+const SearchForm: FC<ISearchFormProps> = ({ handleSubmit, setSearchValue, ...props }) => {
+  const [value, setValue] = useState<string>("");
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
+  const setSearchValueByClick = () => {
+    setSearchValue (value)
+  }
+
   return (
-    <form className={s.search__form} {...props}>
+    <form onSubmit={handleSubmit} className={s.search__form} {...props}>
       <InputSearch handleChange={handleChange} value={value} />
-      <InputSearchBlueButton value="Искать" />
+      <InputSearchBlueButton value="Искать" onClick={setSearchValueByClick} />
     </form>
   );
 };
