6355fd7004003e2f63fc7227a596e13a999c83c7
nat-netl <nat_netl3@bk.ru>
2022-09-16 17:20:45 +0300

    product: correction

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { routesForGuest } from "../../constants/routes";
 import ProductList from "./product list/ProductList";
+import H1ForProducts from "../ui/h1/h1ForCatalog/H1ForProducts";
+import m from "./../../assets/styles/main.module.scss";
+import s from "./products.module.scss";
+import { useLocation } from "react-router-dom";
 
 const Products = () => {
-  return (
-    <ProductList />
-  );
-}
+  const currentRouter = useLocation().pathname;
 
-export default Products
+  return (
+    <div className={s.products}>
+      <div className={m.container}>
+        <header className={s.products__header}>
+          {routesForGuest.map(
+            (route, index) =>
+              route.path === currentRouter && (
+                <H1ForProducts key={index}>{route.title}</H1ForProducts>
+              )
+          )}
+        </header>
+        <ProductList />
+      </div>
+    </div>
+  );
+};
+
+export default Products;
