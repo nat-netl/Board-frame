28f3e296f79defd42a137a8746bef66e7998402a
nat-netl <nat_netl3@bk.ru>
2022-09-16 17:25:47 +0300

    search page

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./search.module.scss";
 import m from "./../../assets/styles/main.module.scss";
 import H1ForProducts from "../../components/ui/h1/h1ForCatalog/H1ForProducts";
 import { useLocation } from "react-router-dom";
 import { routesForGuest } from "../../constants/routes";
 import SearchForm from "../../components/search/searchForm/SearchForm";
+import ProductList from "../../components/products/product list/ProductList";
+import { useAppDispatch } from "../../hooks/redux";
+import { fetchProducts } from "../../redux/actions/ProductsActionCreators";
 
 const Search = () => {
   const currentRouter = useLocation().pathname;
+  const dispatch = useAppDispatch();
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    dispatch(fetchProducts({ name: searchValue }));
+  };
 
   return (
     <div className={s.wrapper}>
@@ -16,16 +26,19 @@ const Search = () => {
           <div className={s.search__column}>
             <header className={s.search__header}>
               {routesForGuest.map(
-                (route) =>
+                (route, index) =>
                   route.path === currentRouter && (
-                    <H1ForProducts>{route.title}</H1ForProducts>
+                    <H1ForProducts key={index}>{route.title}</H1ForProducts>
                   )
               )}
             </header>
-
-            <SearchForm />
+            <SearchForm
+              handleSubmit={handleSubmit}
+              setSearchValue={setSearchValue}
+            />
           </div>
         </div>
+        {searchValue && <ProductList />}
       </div>
     </div>
   );
