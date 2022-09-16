cff91991639ff5f38bd1e13ab1e2e43bc28bf67c
nat-netl <nat_netl3@bk.ru>
2022-09-16 18:29:01 +0300

    new tags

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,21 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import { privateRoutes } from "./constants/routes";
+import m from "./assets/styles/main.module.scss";
 
 const App = () => {
   return (
-    <>
+    <div className={m.wrapper}>
       <Router basename="/">
         <Header />
-        <Routes>
-          {privateRoutes.map((route, index) => (
-            <Route key={index} path={route.path} element={route.element} />
-          ))}
-        </Routes>
+          <Routes>
+            {privateRoutes.map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+            ))}
+          </Routes>
         <Footer />
       </Router>
-    </>
+    </div>
   );
 };
 
