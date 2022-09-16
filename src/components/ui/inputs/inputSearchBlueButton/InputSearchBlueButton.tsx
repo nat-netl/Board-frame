25dd1da9430459d073d0f63ab59dce6ea999b5c7
nat-netl <nat_netl3@bk.ru>
2022-09-16 17:27:41 +0300

    ui:inputs: search blue button

--- src/components/ui/inputs/inputSearchBlueButton/InputSearchBlueButton.tsx
+++ src/components/ui/inputs/inputSearchBlueButton/InputSearchBlueButton.tsx
@@ -3,10 +3,11 @@ import s from "./inputSearchBlueButton.module.scss";
 
 interface IInputSearchBlueButton {
   value: string;
+  onClick: () => void
 }
 
-const InputSearchBlueButton: FC<IInputSearchBlueButton> = ({ value }) => {
-  return <input className={s.blue__button} type="submit" value={value} />;
+const InputSearchBlueButton: FC<IInputSearchBlueButton> = ({ value, onClick, ...props }) => {
+  return <input {...props} className={s.blue__button} onClick={onClick} type="submit" value={value} />;
 };
 
 export default InputSearchBlueButton;
