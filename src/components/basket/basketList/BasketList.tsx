ffad33ee247efd05e7c6b5d5203782676c288e42
nat-netl <71555737+nat-netl@users.noreply.github.com>
2022-09-18 15:54:37 +0300

    components: basket

--- src/components/basket/basketList/BasketList.tsx
+++ src/components/basket/basketList/BasketList.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useAppSelector } from '../../../hooks/redux'
+import BasketProduct from '../basketProduct/BasketProduct'
+import s from "./basketList.module.scss"
 
 const BasketList = () => {
+  const { basket } = useAppSelector(state => state.basket)
+
+  // console.log(basket)
   return (
-    <div>BasketList</div>
+    <div className={s.basket__list}>
+      <table>
+        <tbody className={s.basket__items}>
+          {
+            basket.map((product) => (
+              <BasketProduct
+                key={product.id}
+                id={product.id}
+                name={product.name}
+                brand={product.brand}
+                image={product.image}
+                price={product.price}
+                instock={product.instock}
+              />
+            ))
+          }
+        </tbody>
+      </table>
+
+    </div>
   )
 }
 
--- src/components/basket/basketProduct/BasketProduct.tsx
+++ src/components/basket/basketProduct/BasketProduct.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { useDispatch } from 'react-redux';
+import { priceFormatter } from '../../../helpers/formatter/priceFormatter';
+import basket, { removeFromBasket } from '../../../redux/slices/basket';
+import { IProduct } from '../../../types/product'
+import s from './basketProduct.module.scss'
 
-const BasketProduct = () => {
+const BasketProduct: FC<IProduct> = ({ id, name, image, price, instock }) => {
+  const dispatch = useDispatch();
+  const RUB = priceFormatter("RUB", price);
+
+  const onDeleteItem = () => {
+    dispatch(removeFromBasket({id}))
+  }
+
   return (
-    <div>BasketProduct</div>
+    <tr className={s.basket__item}>
+      <td className={s.item__image}><img src={image} title={name} /></td>
+      <td className={s.item__name}>{name}</td>
+      <td className={s.item__quantity}>
+        <div className={s.quantity}>
+          <button className={s.quantity__btn__plus} type='button'>-</button>
+          <input className={s.quantity__input} maxLength={4} type="text" />
+          <button className={s.quantity__btn__minus} type='button'>+</button>
+        </div>
+      </td>
+      <td className={s.item__price}>{RUB}</td>
+      <td className={s.item__remove}><button className={s.remove__btn} type="button" onClick={onDeleteItem}>×</button></td>
+    </tr>
   )
 }
 
