1070fc8d710c84ab772303f7f7826b799ff7d90e
nat-netl <71555737+nat-netl@users.noreply.github.com>
2022-09-18 16:52:28 +0300

    basket: empty basket

--- src/pages/basket/Basket.tsx
+++ src/pages/basket/Basket.tsx
@@ -2,13 +2,26 @@ import React from "react";
 import s from "./basket.module.scss"
 import m from "./../../assets/styles/main.module.scss"
 import BasketList from "../../components/basket/basketList/BasketList";
+import BasketTotalAmount from "../../components/basket/basketTotalAmount/BasketTotalAmount";
+import { useAppSelector } from "../../hooks/redux";
+import { BasketState } from "../../redux/slices/basket";
 
 const Basket = () => {
+  const { basket, isLoading, error } = useAppSelector<BasketState>(state => state.basket)
+
   return (
     <div className={s.wrapper}>
       <div className={m.container}>
         <div className={s.basket}>
-          <BasketList />
+          {
+            basket.length > 0 ?
+              <>
+                <BasketList basket={basket} isLoading={isLoading} error={error} />
+                <BasketTotalAmount />
+              </>
+              :
+              <h2 className={m.error}>Корзина пуста</h2>
+          }
         </div>
       </div>
     </div>
