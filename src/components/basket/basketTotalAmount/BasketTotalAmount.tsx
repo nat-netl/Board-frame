d4c7e2ba9e64f757d220764dc35bb95f04ecff40
nat-netl <nat_netl3@bk.ru>
2022-09-23 16:54:49 +0300

    total basket: total cost

--- src/components/basket/basketTotalAmount/BasketTotalAmount.tsx
+++ src/components/basket/basketTotalAmount/BasketTotalAmount.tsx
@@ -1,17 +1,33 @@
-import React, { FC } from 'react'
-import { priceFormatter } from '../../../helpers/formatter/priceFormatter'
-import { getBasketTotal } from '../../../redux/slices/basket'
-import { BasketState } from '../../../types/basket'
-import BuyBlueButton from '../../ui/buttons/buyBlueButton/BuyBlueButton'
-import s from "./basketTotalAmount.module.scss"
+import React, { FC } from "react";
+import { priceFormatter } from "../../../helpers/formatter/priceFormatter";
+import { useAppDispatch } from "../../../hooks/redux";
+import {
+  clearBasket,
+  getTotalQuantityItemsBasket,
+} from "../../../redux/slices/basket";
+import { BasketState } from "../../../types/basket";
+import BuyBlueButton from "../../ui/buttons/buyBlueButton/BuyBlueButton";
+import s from "./basketTotalAmount.module.scss";
 
-const BasketTotalAmount: FC<any> = ({ basket }) => {
+const BasketTotalAmount: FC<BasketState> = ({ basket }) => {
+  const totalPrice = getTotalQuantityItemsBasket(basket);
+  const dispatch = useAppDispatch();
+  
+  const onClickBuyProducts = () => {
+    if (window.confirm("Вы действительно хотите купить перечисленные товары?")) {
+      return dispatch(clearBasket());
+    }
+  };
+
   return (
     <div className={s.total}>
-      <div className={s.total__amount}><span className={s.total__amount__text}>Итого</span><span>{priceFormatter("RUB", getBasketTotal(basket))}</span></div>
-      <BuyBlueButton>Купить</BuyBlueButton>
+      <div className={s.total__amount}>
+        <span className={s.total__amount__text}>Итого</span>
+        <span>{priceFormatter("RUB", totalPrice.cost)}</span>
+      </div>
+      <BuyBlueButton onClick={onClickBuyProducts}>Купить</BuyBlueButton>
     </div>
-  )
-}
+  );
+};
 
-export default BasketTotalAmount
+export default BasketTotalAmount;
--- src/redux/slices/basket.ts
+++ src/redux/slices/basket.ts
@@ -1,11 +1,10 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IBaseBasket } from "../../types/basket";
-import { IProduct } from "../../types/product";
 
-interface IBasket {
+export interface IBasket {
   basket: IBaseBasket[];
-  isLoading: boolean,
-  error: string,
+  isLoading: boolean;
+  error: string;
 }
 
 const initialState: IBasket = {
@@ -14,30 +13,80 @@ const initialState: IBasket = {
   error: "",
 };
 
-export const getBasketTotal = (basket: IBasket[]) =>
-  basket?.reduce((amount, item: any) => item.price + amount, 0);
+export const getTotalQuantityItemsBasket = (basket: IBaseBasket[]) => {
+  let total = basket.reduce(
+    (basketTotal, basketItem): any => {
+      const { price, amount } = basketItem;
+      if (amount) basketTotal.cost += price * amount;
+      if (amount) basketTotal.quantity += amount;
+
+      return basketTotal;
+    },
+    {
+      cost: 0,
+      quantity: 0,
+    }
+  );
+
+  return total;
+};
 
 const basketSlice = createSlice({
   name: "basket",
   initialState,
   reducers: {
     addToBasket: (state: IBasket, action: PayloadAction<IBaseBasket>) => {
-      const basketItem = state.basket.find(item => item.id === action.payload.id)
+      const basketItem = state.basket.find(
+        (item) => item.id === action.payload.id
+      );
       if (basketItem) {
         basketItem.amount && basketItem.amount++;
-        console.log(basketItem.amount)
+        console.log(basketItem.amount);
       } else {
-        state.basket.push(action.payload)
+        state.basket.push(action.payload);
       }
     },
-    removeFromBasket: (state, action) => {
-      state.basket = state.basket.filter((item) => item.id !== action.payload.id);
-    }
+    decreaseStuffQuantity: (
+      state: IBasket,
+      action: PayloadAction<IBaseBasket>
+    ) => {
+      const basketItem = state.basket.find(
+        (item) => item.id === action.payload.id
+      );
+      if (basketItem) {
+        basketItem.amount && basketItem.amount--;
+      }
+    },
+    inputStuffQuantity: (
+      state: IBasket,
+      action: PayloadAction<IBaseBasket>
+    ) => {
+      const basketItem = state.basket.find(
+        (item) => item.id === action.payload.id
+      );
+      if (basketItem) {
+        basketItem.amount = action.payload.amount;
+      }
+    },
+    removeFromBasket: (state: IBasket, action) => {
+      state.basket = state.basket.filter(
+        (item) => item.id !== action.payload.id
+      );
+    },
+    clearBasket: (state: IBasket) => {
+      state.basket = [];
+    },
   },
 });
 
 export const cardSelector = {
-  getBasket: (state: any) => state.basket,
+  getBasket: (state: IBasket) => state.basket,
 };
-export const { addToBasket, removeFromBasket } = basketSlice.actions;
+export const {
+  addToBasket,
+  removeFromBasket,
+  decreaseStuffQuantity,
+  inputStuffQuantity,
+  clearBasket,
+} = basketSlice.actions;
 export default basketSlice.reducer;
